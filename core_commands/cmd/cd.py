f7898ca3eacb43ec4a3f8e07e76d90734731f2f1
Ricardo Torrellas <ricardo.torrejas@gmail.com>
2024-04-28 14:54:48 -0400

    v0.2.0

--- core_commands/cmd/cd.py
+++ core_commands/cmd/cd.py
@@ -1,8 +1,7 @@
 from .command_cmd import command_cmd
 
-def cd(pathname,arguments = ""):
+def cd(pathname,arguments = False):
     """
     Change Directory - Select a Folder (and drive)
     """
     command_cmd(f"cd {arguments} {pathname}")
-    pass
