769745f261dd21268371ecfe369ee1dded4a62f6
Ricardo Torrellas <ricardo.torrejas@gmail.com>
2024-05-16 18:31:57 -0400

    v0.4.0:feat

--- core_commands/cmd/command_cmd.py
+++ core_commands/cmd/command_cmd.py
@@ -8,3 +8,5 @@ def basic_execution(command_name,arguments):
         if arguments:
                 return command_cmd(f"{command_name} {arguments}")
         return command_cmd(f"{command_name}")  
+
+# TODO: funcion que te permite ejectar multiples comandos, tanto como cmd con & && y powershell que no tengo idea. o mejor para core-scripts
